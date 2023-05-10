45c3fb5060619843a3a6aec3d0c556c06c1de391
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2023-05-10 13:57:32 -0500

    file

---
+++ python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import hidden_4
+
+if __name__ == "__main__":
+    names = dir(hidden_4)
+    valid_names = [name for name in names if not name.startswith('__')]
+    valid_names.sort()
+    for name in valid_names:
+        print(name)
