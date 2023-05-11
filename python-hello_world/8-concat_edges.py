9188f19909548b37b4cf21609f32697139bb0c44
EveBaker <Evebaker0162@gmail.com>
2023-05-11 09:19:20 -0700

    file

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,1 +1,18 @@
-2
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+
+# define variables a and b
+a = 10
+b = 5
+
+# perform mathematical operations using the imported functions
+sum_result = add(a, b)
+diff_result = sub(a, b)
+prod_result = mul(a, b)
+quo_result = div(a, b)
+
+# print the results using string format
+print("{0} + {1} = {2}".format(a, b, sum_result))
+print("{0} - {1} = {2}".format(a, b, diff_result))
+print("{0} * {1} = {2}".format(a, b, prod_result))
+print("{0} / {1} = {2}".format(a, b, quo_result))
