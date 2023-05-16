47aeb90afc7d79cf860fadb258510f2e33c473b8
EveBaker <Evebaker0162@gmail.com>
2023-05-15 21:11:38 -0700

    file 1

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python3
 
 def print_list_integer(my_list=[]):
-        for num in my_list:
-                    print("{0}".format(num))
+        for item in my_list:
+                    if not isinstance(item, int):
+                                    raise TypeError("All elements of the list must be integers")
+                                        print("{:d}".format(item))
 
-                    my_list = [1, 2, 3, 4, 5]
-                    print_list_integer(my_list)
+                                        # test cases
+                                        my_list = [1, 2, 3, 4, 5]
+                                        print_list_integer(my_list)
+
+                                        my_list = [1]
+                                        print_list_integer(my_list)
+
+                                        my_list = []
+                                        print_list_integer(my_list)
+
+                                        my_list = [1, 2, "H", 9]
+                                        print_list_integer(my_list)
+
