4f852e3ccc2bd7a194723b0acab3dd6a75d9e999
EveBaker <Evebaker0162@gmail.com>
2023-05-15 21:47:38 -0700

    file1

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -3,18 +3,5 @@
 def element_at(my_list, idx):
     if idx < 0 or idx >= len(my_list):
         return None
-    return my_list[idx]
-
-# Testing the function
-test_cases = [
-    ([1, 2, 3], 1),
-    ([1, 2, 3], 0),
-    ([1, 2, 3], 2),
-    ([1, 2, 3], 3),
-    ([1, 2, 3], -1),
-    ([1], 0),
-    ([1], 1)
-]
-
-for lst, idx in test_cases:
-    print(f"List: {lst}, Index: {idx} => Element: {element_at(lst, idx)}")
+    else:
+        return my_list[idx]
