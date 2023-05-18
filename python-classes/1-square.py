def964a3b26f69128acde4ac700072fc47adfd8b
EveBaker <Evebaker0162@gmail.com>
2023-05-18 12:24:47 -0700

    fixed issues

---
+++ python-classes/1-square.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""
+1-square.py: This module defines a Square class that represents a square shape.
+"""
+
+
+class Square:
+    """
+    Represents a square.
+    Attributes:
+    __size (int): The size of the square.
+    """
+
+
+def __init__ (self,size):
+    """
+    Initializes a Square instance.
+    Args:
+    size (int): The size of the square.
+    """
+    self.__size = size
+
+
+
+def dict_(self):
+    """
+    Retrieves the dictionary representation of the Square instance.
+    Returns:
+    dict: The dictionary representation containing the size.
+    """
+    return {'size': self.__size}
+
+
+mysquare = Square(3)
+print(type(mysquare))
+print(mysquare.dict_)
+
+
+mysquare = Square(89)
+print(type(mysquare))
+print(mysquare.dict_)
+
+
+try:
+    print(mysquare.size)
+except Exception as e:
+    print(e)
+
+
+mysquare = Square(3)
+print(type(mysquare))
+print(mysquare.dict_)
+
+
+try:
+    print(type(mysquare))
+except Exception as e:
+    print(e)
