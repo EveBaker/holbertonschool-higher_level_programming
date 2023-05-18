8f318d9c6839b44f823abe692d539f496536bba3
EveBaker <Evebaker0162@gmail.com>
2023-05-18 11:32:39 -0700

    fixed issue

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -2,3 +2,12 @@
 class Square:
     """this is an empty square"""
     pass
+
+def __init__(self):
+    """Initalizes an instace of the square class."""
+    pass
+
+if __name__ == "__main__":
+    my_square = Square()
+print(type(my_square))
+print(my_square.__dict__)
