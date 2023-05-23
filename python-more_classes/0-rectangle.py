5cc1232dca20d75018eab86eddc3ffaf4f97c007
EveBaker <Evebaker0162@gmail.com>
2023-05-23 10:44:10 -0700

    0

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module: 0-rectangle
+Empty Rectangle class
+"""
+class Rectangle:
+    """
+    Rectangle class
+    """
+def __init__(self):
+    """
+    Initalization method
+    """
+    pass
+if __name__=="__main__":
+    my_rectangle = Rectangle()
+    print(type(my_rectangle))
+    print(my_rectangle.__dict__)
