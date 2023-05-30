7037f2d2a8247ff117fc8715132fa478b5882ffa
EveBaker <Evebaker0162@gmail.com>
2023-05-29 19:49:15 -0700

    0

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -4,6 +4,7 @@ class Base():
     __nb_instances = 0
     
 def __init__(self):
+    """Initialize Base instance"""
     Base.__nb_instances += 1
     self.id = Base.__nb_instances
 
@@ -11,10 +12,20 @@ class User(Base):
     """My User class"""
 
     def __init__(self):
+        """Initialize User instance"""
         super().__init__()
         self.id += 99
 
 def lookup(obj):
+    """
+    Return a list of available attributes and methods of an object.
+
+    Args:
+        obj: An object.
+
+    Returns:
+        A list of strings containing the names of attributes and methods of the object.
+    """
     return dir(obj)
 
 b = Base()
