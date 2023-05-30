663de2e03e334bd2f6aea3ebfc16e55c4db85ca1
EveBaker <Evebaker0162@gmail.com>
2023-05-29 20:04:19 -0700

    0

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+class MyList(list):
+    def print_sorted(self):
+        """
+        Print the list in sorted order (ascending sort).
+        """
+        sorted_list = sorted(self)
+        print(sorted_list)
