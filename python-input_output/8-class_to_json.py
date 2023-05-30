0c5bc3d3810bb35e93cb0d6f6b4d188c42746dda
EveBaker <Evebaker0162@gmail.com>
2023-05-30 13:43:34 -0700

    done

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,21 +1,25 @@
-"""Class to JSON Module
+#!/usr/bin/python3
+"""Module 8-class_to_json
 
-This module provides a function to convert an object to a dictionary representation
-with a simple data structure suitable for JSON serialization.
-
+This module provides a function to convert an object of a class to a JSON-serializable dictionary.
 """
 
 def class_to_json(obj):
-    """Converts an object to a dictionary representation for JSON serialization.
+    """
+    Converts an object to a JSON-serializable dictionary.
 
     Args:
-        obj (object): An instance of a class.
+        obj: An instance of a class.
 
     Returns:
-        dict: The dictionary representation of the object.
-
+        A dictionary containing the serialized attributes of the object.
     """
-    if hasattr(obj, '__dict__'):
-        return obj.__dict__.copy()
-    else:
-        return obj
+    if not hasattr(obj, "__dict__"):
+        return {}
+
+    json_dict = {}
+    for key, value in obj.__dict__.items():
+        if not key.startswith("__"):
+            json_dict[key] = value
+
+    return json_dict
