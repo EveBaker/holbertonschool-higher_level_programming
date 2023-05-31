ccf4d002b6ecfe864281dfff36216833361388b9
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2023-05-30 23:53:53 -0500

    edit

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -11,11 +11,5 @@ import json
 def to_json_string(my_obj):
     """
     Return the JSON representation of an object.
-
-    Args:
-        my_obj (obj): The object to be converted to JSON.
-
-    Returns:
-        str: The JSON representation of the object.
     """
     return json.dumps(my_obj)
