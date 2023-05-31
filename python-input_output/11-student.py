611389d3567bb9e24d06485a0b27c9a3d09b5db5
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2023-05-30 23:52:15 -0500

    edit

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -7,21 +7,11 @@ Module: student
 class Student:
     """
     Represents a student.
-
-    Attributes:
-        first_name (str): The first name of the student.
-        last_name (str): The last name of the student.
-        age (int): The age of the student.
     """
 
     def __init__(self, first_name, last_name, age):
         """
         Initializes a new Student instance.
-
-        Args:
-            first_name (str): The first name of the student.
-            last_name (str): The last name of the student.
-            age (int): The age of the student.
         """
         self.first_name = first_name
         self.last_name = last_name
@@ -30,12 +20,6 @@ class Student:
     def to_json(self, attrs=None):
         """
         Retrieves a dictionary representation of the Student instance.
-
-        Args:
-            attrs (list, optional): A list of attribute names to be retrieved.
-                If provided, only the attributes in the list will be included in the dictionary.
-                Defaults to None.
-
         Returns:
             dict: A dictionary representation of the Student instance.
         """
@@ -47,9 +31,6 @@ class Student:
     def reload_from_json(self, json):
         """
         Replaces all attributes of the Student instance with the values from the provided JSON dictionary.
-
-        Args:
-            json (dict): A dictionary containing attribute names as keys and their corresponding values.
         """
         for key, value in json.items():
             setattr(self, key, value)
