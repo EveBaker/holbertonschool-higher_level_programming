f3cbc1b60aee20a7e72e1b1e4efe2a8435fab79e
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2023-05-30 23:54:41 -0500

    edit

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -11,11 +11,5 @@ import json
 def from_json_string(my_str):
     """
     Return an object represented by a JSON string.
-
-    Args:
-        my_str (str): The JSON string representing the object.
-
-    Returns:
-        obj: The Python data structure represented by the JSON string.
     """
     return json.loads(my_str)
