af807d4f36ca9abaac04b4c40beac9f1c5f3e023
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2023-05-30 23:51:19 -0500

    edit

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -7,21 +7,11 @@ Module: student
 class Student:
     """
     Represents a student.
-
-    Attributes:
-        first_name (str): The first name of the student.
-        last_name (str): The last name of the student.
-        age (int): The age of the student.
     """
 
     def __init__(self, first_name, last_name, age):
         """
         Initializes a new Student instance.
-
-        Args:
-            first_name (str): The first name of the student.
-            last_name (str): The last name of the student.
-            age (int): The age of the student.
         """
         self.first_name = first_name
         self.last_name = last_name
@@ -30,14 +20,6 @@ class Student:
     def to_json(self, attrs=None):
         """
         Retrieves a dictionary representation of the Student instance.
-
-        Args:
-            attrs (list, optional): A list of attribute names to be retrieved.
-                If provided, only the attributes in the list will be included in the dictionary.
-                Defaults to None.
-
-        Returns:
-            dict: A dictionary representation of the Student instance.
         """
         if attrs is None:
             return self.__dict__
