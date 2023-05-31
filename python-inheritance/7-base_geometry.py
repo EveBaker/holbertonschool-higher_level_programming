234c8b1cff2a234b1f869f1fd255eee50f828cf2
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2023-05-30 23:46:41 -0500

    edit

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -3,28 +3,13 @@
 7-base_geometry.py:
 This module provides a class called BaseGeometry.
 
-Classes:
     BaseGeometry:
         Base class for other geometry-related classes.
-
-    Methods:
         area(self):
             Compute the area of the geometry.
 
             Raises:
-                Exception: Always raises an Exception with the message "area() is not implemented".
-
-        integer_validator(self, name, value):
-            Validate the value as an integer.
-
-            Args:
-                name (str): The name of the value.
-                value: The value to be validated.
-
-            Raises:
-                TypeError: If the value is not an integer.
-                ValueError: If the value is less than or equal to 0.
-"""
+                Exception: Always raises an Exception with the message "area() is not implemented". """
 
 class BaseGeometry:
     """
@@ -34,8 +19,6 @@ class BaseGeometry:
     def area(self):
         """
         Compute the area of the geometry.
-
-        Raises:
             Exception: Always raises an Exception with the message "area() is not implemented".
         """
         raise Exception("area() is not implemented")
@@ -43,16 +26,12 @@ class BaseGeometry:
     def integer_validator(self, name, value):
         """
         Validate the value as an integer.
-
-        Args:
-            name (str): The name of the value.
-            value: The value to be validated.
-
         Raises:
             TypeError: If the value is not an integer.
             ValueError: If the value is less than or equal to 0.
         """
-        if not isinstance(value, int):
+      
+    if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
