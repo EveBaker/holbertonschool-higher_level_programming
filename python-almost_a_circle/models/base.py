175339560df29ef132415f877d1be01ca5a0bea8
EveBaker <Evebaker0162@gmail.com>
2023-06-04 20:41:08 -0700

    continue

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 class Base:
+    """Base class for managing id attribute"""
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """Initialize Base instance with id"""
         if id is not None:
             self.id = id
         else:
