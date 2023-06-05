db831e5c3e91dfa403d99329861aeb2afdf92809
EveBaker <Evebaker0162@gmail.com>
2023-06-04 20:56:13 -0700

    continue

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
+
+
 """
-Module: rectangle:
+Module: rectangle.py:
 This module defines the Rectangle class.
 """
+
+
 class Rectangle:
     """
     Rectangle class for representing rectangle
     """
+
 
     def __init__(self, width, height):
         """
         Initialize Rectangle instance with width and height"""
 
 
-
-        self.width = width
-        self.height = height
+self.width = width
+self.height = height
