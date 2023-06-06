116d1d9e9643aca186c18c9cf2f4226484f7e576
EveBaker <Evebaker0162@gmail.com>
2023-06-05 20:23:50 -0700

    redo

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,21 +1,16 @@
-#!/usr/bin/python3
-
-"""Rectangle class that inherits from Base."""
-
-
 from models.base import Base
-
 
 class Rectangle(Base):
     """Rectangle class that inherits from Base."""
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """Constructor for Rectangle."""
+        """Constructor for Rectangle class.
+        """
         super().__init__(id)
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
 
     @property
     def width(self):
@@ -25,10 +20,6 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """Setter for the width of the Rectangle."""
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -39,10 +30,6 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         """Setter for the height of the Rectangle."""
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        if value <= 0:
-            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
@@ -53,10 +40,6 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         """Setter for the x (horizontal offset) of the Rectangle."""
-        if not isinstance(value, int):
-            raise TypeError("x must be an integer")
-        if value < 0:
-            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -67,45 +50,4 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         """Setter for the y (vertical offset) of the Rectangle."""
-        if not isinstance(value, int):
-            raise TypeError("y must be an integer")
-        if value < 0:
-            raise ValueError("y must be >= 0")
         self.__y = value
-
-    def area(self):
-        """Returns the area of the Rectangle."""
-        return self.__width * self.__height
-
-    def display(self):
-        """Prints the Rectangle instance with '#' characters in stdout."""
-        for _ in range(self.__y):
-            print()
-        for _ in range(self.__height):
-            print(" " * self.__x + "#" * self.__width)
-
-    def to_dictionary(self):
-        """Returns the dictionary representation of a Rectangle."""
-        return {
-            "id":  self.id,
-            "width": self.width,
-            "height": self.height,
-            "x": self.x,
-            "y": self.y
-        }
-
-    def update(self, *args, **kwargs):
-        """Assigns arguments or key/value arguments to the Rectangle attributes."""
-        attributes = ["id", 'width', "height", 'x', "y"]
-        if args and len(args) > 0:
-            for attribute, value in zip(attributes, args):
-                setattr(self, attribute, value)
-        else:
-            for key, value in kwargs.items():
-                setattr(self, key, value)
-
-    def __str__(self):
-        """Returns a string representation of the Rectangle instance."""
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(
-            self.id, self.__x, self.__y, self.__width, self.__height
-        )
