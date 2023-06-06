e8b0aaffe90500dd7ce705046a2ecafd69dbbe34
EveBaker <Evebaker0162@gmail.com>
2023-06-06 14:54:26 -0700

    next

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -24,10 +24,10 @@ class Base:
             return "[]"
         return json.dumps(list_dictionaries)
 
-    @staticmethod
-    def from_json_string(json_string):
+    @classmethod
+    def from_json_string(cls, json_string):
         """ Static method that returns the list from the JSON string. """
-        if not json_string:
+        if json_string is None or len(json_string) == 0:
             return []
         return json.loads(json_string)
 
