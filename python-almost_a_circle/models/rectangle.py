09d003f981a41c9d6379d397d3cf020763e5bdf2
EveBaker <Evebaker0162@gmail.com>
2023-06-06 10:51:19 -0700

    next

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -121,12 +121,14 @@ class Rectangle(Base):
         for _ in range(self.__height):
             print(" " * self.__x + "#" * self.__width)
 
+
     def __str__(self):
         """
         Returns a string representation of the rectangle.
         """
         return "[Rectangle] ({}) {}/{} - {}/{}".format(
             self.id, self.__x, self.__y, self.__width, self.__height)
+
 
     def update(self, *args, **kwargs):
         """
