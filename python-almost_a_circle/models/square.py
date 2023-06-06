d529ddb908fb4c831fc4e46c148bd289c8851ffc
EveBaker <Evebaker0162@gmail.com>
2023-06-06 12:57:28 -0700

    next

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,31 +1,15 @@
 #!/usr/bin/python3
-
-"""
-This module defines the Square class, which represents a square shape.
-"""
-
+"""Module containing the Square class"""
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """
-    A class representing a square, which is a special type of rectangle.
-    """
+    """Class representing a Square"""
 
     def __init__(self, size, x=0, y=0, id=None):
-        """
-        Initializes a square object.
-
-        Args:
-            size (int): The size of the square.
-            x (int, optional): The x-coordinate of the square's position. Defaults to 0.
-            y (int, optional): The y-coordinate of the square's position. Defaults to 0.
-            id (int, optional): The unique identifier of the square. Defaults to None.
-
-        Raises:
-            ValueError: If size, x, or y is not a positive integer.
-        """
+        """Initialize a Square instance"""
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
+        """Return a string representation of the Square instance"""
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
