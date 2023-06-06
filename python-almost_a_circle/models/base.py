79ec418bf6f364ae46da01c6843e341867aa9a38
EveBaker <Evebaker0162@gmail.com>
2023-06-06 15:04:55 -0700

    fix?please

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -5,11 +5,13 @@ import json
 
 
 class Base:
-    """ Base class will be the 'base' of all other classes in this project. """
+    """ Base class will be the “base” of all other classes in this project.
+    """
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """ class constructor for Base class with optional id attribute. """
+        """ class constructor for Base class with optional id attribute.
+        """
         if id is None:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
@@ -18,22 +20,24 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """ Static method that returns the JSON string representation
-        of list_dictionaries. """
-        if list_dictionaries is None or len(list_dictionaries) == 0:
+        """ Static method that Returns: the JSON string representation
+        """
+        if not list_dictionaries:
             return "[]"
         return json.dumps(list_dictionaries)
 
-    @classmethod
-    def from_json_string(cls, json_string):
-        """ Static method that returns the list of the JSON string. """
-        if json_string is None or len(json_string) == 0:
+    @staticmethod
+    def from_json_string(json_string):
+        """ Static method that Returns: the list of the JSON string
+        """
+        if not json_string:
             return []
         return json.loads(json_string)
 
     @classmethod
     def save_to_file(cls, list_objs):
-        """ Class method that writes the JSON representation of a string to a file. """
+        """ Class method writes the JSON representation of a string to a file.
+        """
         filename = cls.__name__ + ".json"
         if not list_objs:
             list_objs = []
@@ -47,33 +51,31 @@ class Base:
 
     @classmethod
     def create(cls, **dictionary):
-        """ Class method that returns an instance of a class. """
-        # create a dummy instance for the respective class to be created
+        """ Class method that returns an instance of a class
+            with all attribute values already set."""
+        # create a dummy instance for either cls to be created
         if cls.__name__ == "Rectangle":
             dummy = cls(1, 1)
-        elif cls.__name__ == "Square":
+        if cls.__name__ == "Square":
             dummy = cls(1)
-        else:
-            dummy = cls()
         # update the dummy instance with the dictionary
         dummy.update(**dictionary)
-        # return the created instance
+        # we created the instance of the class with all attrs set
         return dummy
 
     @classmethod
     def load_from_file(cls):
-        """ Class method that returns a list of instances. """
+        """ Class method that Returns: A list of instances.
+        """
         # create a class name JSON file
         filename = cls.__name__ + ".json"
-        instances = []
+        # try to open the file
         try:
             with open(filename, "r") as file:
                 # read the file and convert to list of dictionaries
                 list_dicts = cls.from_json_string(file.read())
                 # convert list of dictionaries to list of instances
-                for dictionary in list_dicts:
-                    instance = cls.create(**dictionary)
-                    instances.append(instance)
+                return [cls.create(**dictionary) for dictionary in list_dicts]
+        # if empty file or file does not exist return empty list
         except FileNotFoundError:
             return []
-        return instances
