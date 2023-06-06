1026d29cfc8aa830e3bdeb479fe6460676a5d652
EveBaker <Evebaker0162@gmail.com>
2023-06-06 14:49:21 -0700

    next

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -20,10 +20,9 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """ Static method that Returns: the JSON string representation
-        of a list of dictionaries.
-        """
-        if not list_dictionaries:
+        """ Static method that returns the JSON string representation
+        of a list of dictionaries. """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
 
