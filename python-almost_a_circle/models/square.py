5bb543b6775360e0871f442d09686e3c18b11553
EveBaker <Evebaker0162@gmail.com>
2023-06-06 12:03:09 -0700

    next

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """
-This module defines the Square class, which inherits from Rectangle.
+This module defines the Square class, which represents a square shape.
 """
 
 from models.rectangle import Rectangle
@@ -9,45 +9,36 @@ from models.rectangle import Rectangle
 
 class Square(Rectangle):
     """
-    A class representing a square.
+    A class representing a square, which is a special type of rectangle.
     """
 
     def __init__(self, size, x=0, y=0, id=None):
         """
         Initializes a square object.
         """
-
         self.width = size
         self.height = size
         self.x = x
         self.y = y
         self.id = id
-        self.validate_positive_integer(size, "size")
-        self.validate_positive_integer(x, "x")
-        self.validate_positive_integer(y, "y")
 
     def __str__(self):
         """
         Returns a string representation of the square.
         """
-
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
+    def area(self):
+        """
+        Calculates and returns the area of the square.
+        """
+        return self.width * self.width
 
     def display(self):
         """
-        Displays a visual representation of the square.
+        Prints a visual representation of the square using '#' characters.
         """
-
         for _ in range(self.y):
             print()
         for _ in range(self.height):
             print(" " * self.x + "#" * self.width)
-
-    @staticmethod
-    def validate_positive_integer(value, name):
-        """
-        Validates if the given value is a positive integer.
-        """
-
-        if not isinstance(value, int) or value <= 0:
-            raise ValueError(f"{name} must be a positive integer.")
