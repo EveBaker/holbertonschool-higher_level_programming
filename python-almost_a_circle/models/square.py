e22de677705995bb15a925460b347bdf75f22a68
EveBaker <Evebaker0162@gmail.com>
2023-06-06 13:01:49 -0700

    next

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -16,3 +16,15 @@ class Square(Rectangle):
     def __str__(self):
         """Return a string representation of the Square instance"""
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
+
+@property
+def size(self):
+        """Getter for the size attribute"""
+        return self.width
+
+@size.setter
+def size(self, value):
+        """Setter for the size attribute"""
+        self.width = value
+        self.height = value
