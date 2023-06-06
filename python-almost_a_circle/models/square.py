389fa0407eaca46f4bb08bef69e382e9f25b3aa5
EveBaker <Evebaker0162@gmail.com>
2023-06-06 11:29:25 -0700

    next

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -3,18 +3,18 @@
 from models.rectangle import Rectangle
 
 class Square(Rectangle):
-        """ Square class that inherits from Rectangle class."""
-        def __init__(self, size, x=0, y=0, id=None):
+    """ Square class that inherits from Rectangle class."""
+    def __init__(self, size, x=0, y=0, id=None):
             super().__init__(size, size, x, y, id)
 
-        def __str__(self):
-            return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+    def __str__(self):
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
 
-        def area(self):
-            return self.width * self.height
+    def area(self):
+        return self.width * self.height
 
-        def display(self):
-            for _ in range(self.y):
-                print()
-            for _ in range(self.height):
-                print(" " * self.x + "#" * self.width)
+    def display(self):
+        for _ in range(self.y):
+            print()
+        for _ in range(self.height):
+            print(" " * self.x + "#" * self.width)
