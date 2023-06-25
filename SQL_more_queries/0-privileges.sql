7fa6fc3b1043ade23f77e8f5092d46f2f49fe100
EveBaker <Evebaker0162@gmail.com>
2023-06-25 15:01:34 -0700

    more sql

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,7 +1,4 @@
 -- Script that lists all privileges of the MySQL users
 -- Query to list all privileges (GRANT) of the MySQL users
-CREATE USER 'user_0d_1'@'localhost';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-
-CREATE USER 'user_0d_2'@'localhost';
-GRANT SELECT, INSERT ON user_2_db.* TO 'user_0d_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
