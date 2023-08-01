7b5cdb9a3a2a0b6d02dfef905fd1d30b43e73c98
EveBaker <EveBaker0162@gmail.com>
2023-08-01 11:35:51 -0700

    javascript

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-let languages = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+let languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let i;
 for (i = 0; i < languages.length; i++) {
     console.log(languages[i]);
