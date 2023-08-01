c6fde15ee8f58d8bbe63210abf65c96bf27cc841
EveBaker <EveBaker0162@gmail.com>
2023-08-01 11:11:27 -0700

    javascript

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const process = require('process');
+let resp;
+if (process.argv.length === 3) {
+    resp = 'Argumrnt found';
+}
+else if (process.argv.length < 3) {
+    resp = 'No argument';
+}
+else {
+    resp = 'Arguments found';
+}
+console.log(resp);
