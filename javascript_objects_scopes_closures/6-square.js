6756668116b5e13271b2686184e1f67d3ed242a9
EveBaker <EveBaker0162@gmail.com>
2023-08-01 14:13:41 -0700

    javascript

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 const square = require('./5-square');
 class Square extends square {
-    constructor (size) {
-        super(size, size);
+  constructor (size) {
+    super(size, size);
+  }
+  charPrint (c) {
+    if (typeof c === 'undefined') {
+      c = 'X';
     }
-    charPrint (c) {
-        if (typeof c === 'undefined') {
-            c = 'X';
-        }
-        this.charPrint(c);
-    }
+    this.print(c);
+  }
 }
 module.exports = Square;
