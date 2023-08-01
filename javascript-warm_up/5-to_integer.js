4b2ff43dc8c57881a6d79544524c24c510f3eb79
EveBaker <EveBaker0162@gmail.com>
2023-08-01 11:26:35 -0700

    javascript

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -6,7 +6,7 @@ resp = 'Not a number';
 if (process.argv.length > 2) {
     num = parseInt(process.argv[2]);
     if (!isNaN(num)) {
-        resp = 'My number:' + String(num);
+        resp = 'My number: ' + String(num);
     }
 }
 console.log(resp);
