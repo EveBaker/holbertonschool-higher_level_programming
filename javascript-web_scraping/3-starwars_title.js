71b0b51b3956f026efae973490e22462b9b90414
EveBaker <EveBaker0162@gmail.com>
2023-08-08 11:16:13 -0700

    java

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,9 +1,15 @@
 #!/usr/bin/node
 const request = require('request');
-const id = process.argv[2];
-request('http://swapi.co/api/films/' + id + '/', function (error, response, body) {
-  if (error == null) {
-    const json = JSON.parse(body);
-    console.log(json.title);
+const movieID = process.argv[2];
+const urlEndpoint = `https://swapi-api.hbtn.io/api/films/${movieID}`;
+
+request(urlEndpoint, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error.message);
+  } else if (response.statusCode !== 200) {
+    console.error('Error:', body);
+  } else {
+    const movieData = JSON.parse(body);
+    console.log(movieData.title);
   }
 });
